d53f7ff3cef9907bb5ed181345efbda832aa4932
Edwxd <101204772+Edwxd@users.noreply.github.com>
2024-12-02 18:24:33 -0500

    Re-did the whole back end for viewing all articles as previous branch had issues

--- computer_alerts-be/src/main/java/com/calerts/computer_alertsbe/articleservice/businesslayer/ArticleServiceImpl.java
+++ computer_alerts-be/src/main/java/com/calerts/computer_alertsbe/articleservice/businesslayer/ArticleServiceImpl.java
@@ -1,12 +1,14 @@
 package com.calerts.computer_alertsbe.articleservice.businesslayer;
 
 
+
 import com.calerts.computer_alertsbe.articleservice.dataaccesslayer.ArticleRepository;
 import com.calerts.computer_alertsbe.articleservice.presentationlayer.ArticleResponseModel;
 import com.calerts.computer_alertsbe.utils.EntityModelUtil;
 import com.calerts.computer_alertsbe.utils.exceptions.NotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @Service
@@ -14,9 +16,17 @@ public class ArticleServiceImpl implements ArticleService {
 
     @Autowired
     private ArticleRepository articleRepository;
+
+    @Override
+    public Flux<ArticleResponseModel> getAllArticleForSpecificSport(String tagName) {
+        return articleRepository.findAllArticleByTags(tagName)
+                .map(EntityModelUtil::toArticleResponseModel);
+
+    }
+
     @Override
     public Mono<ArticleResponseModel> getArticleByArticleId(String articleId) {
-        return articleRepository.findArticleByArticleId(articleId)
+        return articleRepository.findArticleByArticleIdentifier(articleId)
                 .switchIfEmpty(Mono.error(new NotFoundException("No article with this id was found" + articleId)))
                 .map(EntityModelUtil::toArticleResponseModel);
     }
--- computer_alerts-be/src/main/java/com/calerts/computer_alertsbe/articleservice/dataaccesslayer/ArticleRepository.java
+++ computer_alerts-be/src/main/java/com/calerts/computer_alertsbe/articleservice/dataaccesslayer/ArticleRepository.java
@@ -1,10 +1,13 @@
 package com.calerts.computer_alertsbe.articleservice.dataaccesslayer;
 
+
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
-import org.springframework.stereotype.Repository;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 public interface ArticleRepository extends ReactiveMongoRepository<Article, String> {
-        Mono<Article> findArticleByArticleId(String articleId);
+        Mono<Article> findArticleByArticleIdentifier(String articleId);
+
+        Flux<Article> findAllArticleByTags(String tagName);
 
 }
