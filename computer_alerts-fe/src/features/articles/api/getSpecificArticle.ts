76e598544ddd13580bdc3cde75f23b15b9df68b9
Ilyass Gouriny <2131685@champlaincollege.qc.ca>
2024-12-02 22:13:39 -0500

    Made the front-end fetch data from the backend.

---
+++ computer_alerts-fe/src/features/articles/api/getSpecificArticle.ts
@@ -0,0 +1,8 @@
+// src/services/articleService.ts
+import axiosInstance from "../models/articlesAxiosInstance";
+import { Article } from "../models/Article";
+
+export const fetchArticleByArticleId = async (articleId: string): Promise<Article> => {
+  const response = await axiosInstance.get<Article>(`/articles/${articleId}`);
+  return response.data;
+};
