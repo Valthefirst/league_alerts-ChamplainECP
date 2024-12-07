11ab3c8f6bd9dda99c28784ed6cb5f8d397accd2
Edwxd <101204772+Edwxd@users.noreply.github.com>
2024-12-06 23:14:35 -0500

    Made the nav bar navigation to the NBA page work. Fetch article is still being worked on as they still do not appear on the page. (Articles not found).

---
+++ computer_alerts-fe/src/features/articles/components/ArticleCard.tsx
@@ -0,0 +1,65 @@
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { Article } from "../models/Article";
+import { fetchArticleByTag } from "../api/getAllArticleBySports";
+
+
+const ArticleCard: React.FC = () => {
+
+    const { tag } = useParams<{ tag: string }>(); // The tag passed via route
+    const [article, setArticles] = useState<Article[]>([]);
+    const [loading, setLoading] = useState<boolean>(true);
+    const [error, setError] = useState<string | null>(null);
+
+    useEffect(() => {
+        const loadArticles = async () => {
+            try {
+                if (tag) {
+                    const data = await fetchArticleByTag(tag);
+                    setArticles(data);
+                }
+            } catch (err) {
+                setError("Failed to fetch the articles");
+            } finally {
+                setLoading(false);
+            }
+        };
+
+        loadArticles();
+    }, [tag]);
+
+    if (loading) return <p>Loading...</p>;
+    if (error) return <p>{error}</p>;
+
+    return article.length > 0 ? (
+        <div className="article-card">
+            {article.map((article) => (
+                <div key={article.id} className="article-card-content">
+                    <h3>{article.title}</h3>
+                    <p>
+                        <strong>
+                            Tags:
+                        </strong>
+                        {article.tags}
+                    </p>
+                    <p>
+                        <strong>
+                            Date Posted:
+                        </strong>
+                        {article.timePosted}
+                    </p>
+                </div>
+            ))}
+        </div>
+    ) : (
+        <p>No articles found</p>
+    );
+  
+}
+
+export default ArticleCard;
+
+   
+
+
+   
