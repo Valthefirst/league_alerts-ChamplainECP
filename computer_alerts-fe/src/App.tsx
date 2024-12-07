247de212f00c4bcbeba3681cb67751e526596e98
Edwxd <101204772+Edwxd@users.noreply.github.com>
2024-12-06 23:14:35 -0500

    Made the nav bar navigation to the NBA page work. Fetch article is still being worked on as they still do not appear on the page. (Articles not found).

--- computer_alerts-fe/src/App.tsx
+++ computer_alerts-fe/src/App.tsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import './App.css';
 import { AppRoutePaths } from './shared/models/path.routes';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
@@ -6,17 +5,8 @@ import AppNavBar from './layouts/AppNavBar';
 import HomePage from './pages/Home/HomePage';
 import ArticleDetails from 'features/articles/components/ArticleDetails';
 import AuthorPage from 'pages/AuthorPage';
-=======
-import "./App.css";
-import { AppRoutePaths } from "./shared/models/path.routes";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import AppNavBar from "./layouts/AppNavBar";
-import HomePage from "./pages/Home/HomePage";
-import ArticleDetails from "features/articles/components/ArticleDetails";
 import ArticlesPage from "./pages/ArticlePages/ArticlePage";
 
->>>>>>> 2f61f6c (fixed fetch articles by tag (sports). Dummy data added (2 articles per sport))
-// import LogoutButton from "./Logout";
 
 function App(): JSX.Element {
   return (
