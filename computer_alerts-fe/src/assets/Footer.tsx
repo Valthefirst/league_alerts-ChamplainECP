115bdcbc887c7cd1ec340e631f2c48687a8d9ec4
J91-cloud <jessygjerek2@gmail.com>
2024-12-08 18:58:40 -0500

    Start of CSS createUser

---
+++ computer_alerts-fe/src/assets/Footer.tsx
@@ -0,0 +1,14 @@
+import styles from "../assets/Footer.module.css"
+import LeagueImage from "../assets/LeagueAlertsImg.jpg"
+
+export default function Footer(){
+    return(
+        // <div className="container">
+        //     <div className="img"><LeagueImage/></div>
+        //     <p>This is your footer</p>
+        // </div>
+        <h1>Hello</h1>
+        
+
+    );
+}
