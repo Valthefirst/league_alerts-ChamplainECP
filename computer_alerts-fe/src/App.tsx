303ec9ffb1601b319237c3f949c87441912b623c
Edwxd <101204772+Edwxd@users.noreply.github.com>
2024-12-08 17:39:17 -0500

    fixed fetch articles by tag (sports). Dummy data added (2 articles per sport)

--- computer_alerts-fe/src/App.tsx
+++ computer_alerts-fe/src/App.tsx
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import './App.css';
 import { AppRoutePaths } from './shared/models/path.routes';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
@@ -5,6 +6,16 @@ import AppNavBar from './layouts/AppNavBar';
 import HomePage from './pages/Home/HomePage';
 import ArticleDetails from 'features/articles/components/ArticleDetails';
 import AuthorPage from 'pages/AuthorPage';
+=======
+import "./App.css";
+import { AppRoutePaths } from "./shared/models/path.routes";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import AppNavBar from "./layouts/AppNavBar";
+import HomePage from "./pages/Home/HomePage";
+import ArticleDetails from "features/articles/components/ArticleDetails";
+import ArticlesPage from "./pages/ArticlePages/ArticlePage";
+
+>>>>>>> 2f61f6c (fixed fetch articles by tag (sports). Dummy data added (2 articles per sport))
 // import LogoutButton from "./Logout";
 
 function App(): JSX.Element {
