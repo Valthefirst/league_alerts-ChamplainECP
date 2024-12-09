97e1a148c089480fd2a7f42c8463a05d6fda49a9
J91-cloud <jessygjerek2@gmail.com>
2024-12-09 13:24:53 -0500

    Diagrams

--- computer_alerts-fe/src/features/articles/components/ArticleDetails/ArticleDetails.tsx
+++ computer_alerts-fe/src/features/articles/components/ArticleDetails/ArticleDetails.tsx
@@ -72,8 +72,6 @@ const ArticleDetails: React.FC = () => {
         </div>
       </div>
 
-      <h1>This is the article ID: {article.articleId || "No ID available"}</h1>
-
       <h1 className="article-title">{article.title}</h1>
       <p className="article-body">{article.body}</p>
 
