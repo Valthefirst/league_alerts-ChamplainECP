87f153ff73ee429d4fa74d2cb255cc723da7df08
Edwxd <101204772+Edwxd@users.noreply.github.com>
2024-12-12 17:52:50 -0500

    commented un-used configs

--- computer_alerts-be/src/main/java/com/calerts/computer_alertsbe/utils/configAPI/CloudinaryConfig.java
+++ computer_alerts-be/src/main/java/com/calerts/computer_alertsbe/utils/configAPI/CloudinaryConfig.java
@@ -7,12 +7,12 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class CloudinaryConfig {
-    @Bean
-    public Cloudinary cloudinary() {
-        return new Cloudinary(ObjectUtils.asMap(
-                "cloud_name", "ddihej6gw",
-                "api_key", "774976436626872",
-                "api_secret", "5PuaO04mxzwACVBnxmwrYxOBT0A"
-        ));
-    }
+//    @Bean
+//    public Cloudinary cloudinary() {
+//        return new Cloudinary(ObjectUtils.asMap(
+//                "cloud_name", "ddihej6gw",
+//                "api_key", "774976436626872",
+//                "api_secret", "5PuaO04mxzwACVBnxmwrYxOBT0A"
+//        ));
+//    }
 }
