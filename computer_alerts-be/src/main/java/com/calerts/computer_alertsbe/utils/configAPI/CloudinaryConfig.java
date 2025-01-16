8aaad290b81e97dd9b909ce615351cdd375c21c2
Edwxd <101204772+Edwxd@users.noreply.github.com>
2025-01-16 14:26:07 -0500

    Modified clodinary access with .env variables for deployment

--- computer_alerts-be/src/main/java/com/calerts/computer_alertsbe/utils/configAPI/CloudinaryConfig.java
+++ computer_alerts-be/src/main/java/com/calerts/computer_alertsbe/utils/configAPI/CloudinaryConfig.java
@@ -4,11 +4,15 @@ package com.calerts.computer_alertsbe.utils.configAPI;
 import com.cloudinary.Cloudinary;
 import com.cloudinary.utils.ObjectUtils;
 import io.github.cdimascio.dotenv.Dotenv;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class CloudinaryConfig {
+
+    @Value("${cloudinary.url}")
+    private String CLOUDINARY_SECRET_URL;
     @Bean
     public Cloudinary cloudinary() {
         String dotenvPath = System.getenv("DOTENV_PATH");
@@ -16,7 +20,7 @@ public class CloudinaryConfig {
         Dotenv dotenv = Dotenv.configure()
                 .directory(dotenvPath != null ? dotenvPath : ".")
                 .load();
-        return new Cloudinary(dotenv.get("CLOUDINARY_URL"));
+        return new Cloudinary(CLOUDINARY_SECRET_URL);
 
     }
 }
