ef4f660b439d0b005dcae7fc086555ab693a8cf8
Valentine Nneji <ama.val100@gmail.com>
2025-01-16 08:16:00 -0500

    changed the calls the backend

--- computer_alerts-fe/src/features/comments/model/commentsAxiosInstance.ts
+++ computer_alerts-fe/src/features/comments/model/commentsAxiosInstance.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:8080/api/v1/interactions/comments",
+  baseURL: "http://dolphin-app-sxvxi.ondigitalocean.app/api/v1/interactions/comments",
   headers: {
     "Content-Type": "application/json",
     Accept: "application/json",
