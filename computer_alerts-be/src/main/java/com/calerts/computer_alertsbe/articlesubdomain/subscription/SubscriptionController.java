951d8700accf45550bc3c6db5700b63286c4d882
Ilyass Gouriny <2131685@champlaincollege.qc.ca>
2025-01-28 08:32:29 -0500

    something in subscription controller

--- computer_alerts-be/src/main/java/com/calerts/computer_alertsbe/articlesubdomain/subscription/SubscriptionController.java
+++ computer_alerts-be/src/main/java/com/calerts/computer_alertsbe/articlesubdomain/subscription/SubscriptionController.java
@@ -3,7 +3,6 @@ package com.calerts.computer_alertsbe.articlesubdomain.subscription;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import reactor.core.publisher.Mono;
 
 import java.util.List;
 
