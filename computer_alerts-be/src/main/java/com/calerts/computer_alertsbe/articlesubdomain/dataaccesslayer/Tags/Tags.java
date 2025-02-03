89ea9ee5e97bb9f6ad1c8c6dfaad7894baed750d
Edwxd <101204772+Edwxd@users.noreply.github.com>
2025-02-03 11:31:23 -0500

    Maybe fix plz help me !

--- computer_alerts-be/src/main/java/com/calerts/computer_alertsbe/articlesubdomain/dataaccesslayer/Tags/Tags.java
+++ computer_alerts-be/src/main/java/com/calerts/computer_alertsbe/articlesubdomain/dataaccesslayer/Tags/Tags.java
@@ -2,12 +2,14 @@ package com.calerts.computer_alertsbe.articlesubdomain.dataaccesslayer.Tags;
 
 import jakarta.persistence.Embedded;
 import lombok.*;
+import org.springframework.data.mongodb.core.mapping.Document;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
 @EqualsAndHashCode
+@Document(collection = "tags")
 public class Tags {
 
     @Embedded
