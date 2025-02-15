ac9af59840c3252d3638a537dc34ee6a845d201d
J91-cloud <jessygjerek2@gmail.com>
2025-02-15 09:31:08 -0500

    Playwright Tests

---
+++ computer_alerts-fe/tests/Readers/GoogleFacebook.spec.ts
@@ -0,0 +1,12 @@
+import { test, expect } from '@playwright/test';
+
+test('test', async ({ page }) => {
+  await page.goto('http://localhost:3000/');
+  await page.locator('#basic-navbar-nav').getByRole('img').click();
+  await page.getByRole('link', { name: 'Create Account / Sign In' }).click();
+  const page1Promise = page.waitForEvent('popup');
+  await page.getByRole('button', { name: 'Log In' }).click();
+  const page1 = await page1Promise;
+  await expect(page1.getByRole('button', { name: 'Continue with Google' })).toBeVisible();
+  await expect(page1.getByRole('button', { name: 'Continue with Google' })).toBeVisible();
+});
